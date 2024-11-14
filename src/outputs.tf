0f131452734bd9d0bda9ec424323f1d58d07087e
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:21:49 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,9 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "node_pools" {
+  value       = kubernetes_manifest.node_pool
+  description = "Deployed Karpenter NodePool"
+}
+
+output "ec2_node_classes" {
+  value       = kubernetes_manifest.ec2_node_class
+  description = "Deployed Karpenter EC2NodeClass"
 }
