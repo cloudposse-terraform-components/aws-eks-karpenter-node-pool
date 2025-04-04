be9447dd6bac778705e7b48c8218c6bbbfd4d315
Igor Rodionov <goruha@gmail.com>
2025-04-05 00:35:41 +0200

    Added tests

--- src/remote-state.tf
+++ src/remote-state.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
-  version = "1.5.0"
+  version = "1.8.0"
 
   component = var.eks_component_name
 
@@ -16,7 +16,7 @@ module "eks" {
 
 module "vpc" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
-  version = "1.5.0"
+  version = "1.8.0"
 
   component = "vpc"
 
