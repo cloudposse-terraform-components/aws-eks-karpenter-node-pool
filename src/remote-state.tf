5b629213da2b19641b05b50b0b6395558869ed1e
Igor Rodionov <496956+goruha@users.noreply.github.com>
2026-04-11 02:05:59 +0300

    Add auto mode configuration to EKS settings

--- src/remote-state.tf
+++ src/remote-state.tf
@@ -17,6 +17,8 @@ module "eks" {
     eks_cluster_identity_oidc_issuer       = coalesce(var.eks.eks_cluster_identity_oidc_issuer, "deleted")
     karpenter_iam_role_name                = var.eks.karpenter_iam_role_name
     karpenter_node_role_arn                = coalesce(var.eks.karpenter_node_role_arn, "deleted")
+    auto_mode_node_role_name               = coalesce(var.eks.auto_mode_node_role_name, "deleted")
+    auto_mode_enabled                      = coalesce(var.eks.auto_mode_enabled, false)
   }
 
   context = module.this.context
