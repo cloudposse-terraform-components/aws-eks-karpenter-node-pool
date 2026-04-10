b868e837fdfa8330960548bd732bf65793420cb9
Igor Rodionov <496956+goruha@users.noreply.github.com>
2026-04-10 22:19:30 +0300

    Remove account_map_enabled variable
    Removed the account_map_enabled variable which controlled the enabling of account map and remote state lookups.

--- src/variables.tf
+++ src/variables.tf
@@ -3,17 +3,6 @@ variable "region" {
   description = "AWS Region"
 }
 
-variable "account_map_enabled" {
-  type        = bool
-  description = <<-EOT
-    Enable account map and remote state lookups.
-    When `true`, fetch EKS cluster and VPC information from Terraform remote state.
-    When `false`, use the `eks` and `vpc` variables to provide values directly.
-    EOT
-  default     = true
-  nullable    = false
-}
-
 variable "eks_component_name" {
   type        = string
   description = <<-EOT
